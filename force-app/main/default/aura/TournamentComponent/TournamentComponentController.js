52d66412a7f22a60174462ef7ab7e1765ba3648b
selemanka <31805806+selemanka@users.noreply.github.com>
2019-06-28 10:24:50 +0300

    initial commit

---
+++ force-app/main/default/aura/TournamentComponent/TournamentComponentController.js
@@ -0,0 +1,17 @@
+({    
+        getTournamentsList : function(component, event, helper) {
+          var action = component.get("c.getTournaments");  
+       	  action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === 'SUCCESS') {
+                var tournamentList = response.getReturnValue();
+                component.set("v.tournamentList",tournamentList);
+            }
+            else {
+                alert('Error in getting data');
+            }
+        });
+        $A.enqueueAction(action);
+        }
+
+})
