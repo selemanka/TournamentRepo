9e1dfda164b2782de67a3c2ae949bef0fe695518
selemanka <31805806+selemanka@users.noreply.github.com>
2019-07-01 15:54:48 +0300

    tab_for_players

--- force-app/main/default/aura/TournamentComponent/TournamentComponentController.js
+++ force-app/main/default/aura/TournamentComponent/TournamentComponentController.js
@@ -31,6 +31,20 @@
             }
         });
         $A.enqueueAction(action);
-
-    }
+    },
+    
+    getPlayersList : function(component, event, helper) {
+          var action = component.get("c.getPlayers");  
+       	  action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === 'SUCCESS') {
+				var playerList = response.getReturnValue();
+                component.set("v.playerList",playerList);
+            }
+            else {
+                alert('Error in getting data');
+            }
+        });
+        $A.enqueueAction(action);
+       },
 })
