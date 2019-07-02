896a84c85fd1a705bdb18239164238c8fd71482d
selemanka <31805806+selemanka@users.noreply.github.com>
2019-07-02 06:23:10 +0300

    pop-up

---
+++ force-app/main/default/aura/CreateTournament/CreateTournamentController.js
@@ -0,0 +1,22 @@
+({
+	   handleSubmit: function(cmp, event, helper) {
+        //cmp.set('v.disabled', true);
+        console.log('submit');
+        var record = event.getParam("response"); 
+           console.log(JSON.stringify(record));
+           cmp.get("v.tournament").Id = record.id;
+           cmp.get("v.tournament").Name = record.fields.Name.value;
+           cmp.get("v.tournament").Type__c = record.fields.Type__c.value;
+           cmp.get("v.tournament").Status__c = record.fields.Status__c.value;
+           cmp.get("v.tournament").Date__c = record.fields.Date__c.value;
+           
+           console.log(JSON.stringify(cmp.get("v.tournament")));
+    },
+    openModel: function(component, event, helper) {
+      component.set("v.isModalOpen", true);
+   },
+  
+   closeModel: function(component, event, helper) {
+      component.set("v.isModalOpen", false);
+   }
+})
